d5ea3ed4401933f33de12c6f5d7690fd7a181347
Sergey <derevankosergej0@gmail.com>
2021-01-29 21:09:21 +0200

    Функционал регистрации

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -39,3 +39,48 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class RegistrationForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.CharField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+        self.fields['confirm_password'].label = 'Подтвердите пароль'
+        self.fields['phone'].label = 'Номер телефона'
+        self.fields['first_name'].label = 'Имя'
+        self.fields['last_name'].label = 'Фамилия'
+        self.fields['address'].label = 'Адрес'
+        self.fields['email'].label = 'Электронная почта'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net']:
+            raise forms.ValidationError('Регистрация с таким доменом недоступна')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Данный почтовый ящик зарегестрирован в системе')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Имя занято')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'confirm_password', 'first_name', 'last_name', 'phone', 'address', 'email')
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,7 +6,7 @@ from django.views.generic import DetailView, View
 from django.http import HttpResponseRedirect
 from .models import *
 from .mixins import CartMixin
-from .forms import OrderForm, LoginForm
+from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
 
 
@@ -157,4 +157,45 @@ class LoginView(CartMixin, View):
             if user:
                 login(request, user)
                 return HttpResponseRedirect('/')
-        return render(request, 'mainapp/login.html', {'form': form, 'cart': self.cart})
+        context = {
+            'form': form,
+            'cart': self.cart
+        }
+        return render(request, 'mainapp/login.html', context)
+
+
+class RegistrationView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {
+            'form': form,
+            'categories': categories,
+            'cart': self.cart
+        }
+        return render(request, 'mainapp/registration.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.email = form.cleaned_data['email']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+
+            Customer.objects.create(user=new_user, phone=form.cleaned_data['phone'],
+                                    address=form.cleaned_data['address'])
+
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, user)
+            return HttpResponseRedirect('/')
+        context = {
+            'form': form,
+            'cart': self.cart
+        }
+        return render(request, 'mainapp/registration.html', context)
