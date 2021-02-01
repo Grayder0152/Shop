637755801806d869a5bc5cc1e41a7f5a553468ad
Sergey <derevankosergej0@gmail.com>
2021-02-01 14:33:41 +0200

    Добавление локики создание категории

--- specs/urls.py
+++ specs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BaseSpacView
+from .views import BaseSpacView,NewCategoryView
 
 
 urlpatterns = [
     path('', BaseSpacView.as_view(), name='base-spec'),
+    path('new-category', NewCategoryView.as_view(), name='new_category'),
 ]
